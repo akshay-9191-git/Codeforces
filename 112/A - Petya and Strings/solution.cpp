972ac40ebd31524e46a80c838a5e726d04238e37
AKSHAY KUMAR <akshay.2026.91@gmail.com>
2026-09-01 19:20:57 +0530

    Add Petya and Strings [A] from Codeforces

---
+++ 112/A - Petya and Strings/solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+ 
+int main(){
+    string x,y;
+    cin>>x>>y;
+    transform(x.begin(),x.end(),x.begin(), :: tolower);
+    transform(y.begin(),y.end(),y.begin(), :: tolower);
+    if(x == y){
+        cout<<0;
+    }else if(x<y){
+        cout<<-1;
+    }else{
+        cout<<1;
+    }
+}
