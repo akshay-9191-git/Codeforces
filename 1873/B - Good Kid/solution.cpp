de6657d024ff8a197a78839706c1ef50308c3987
AKSHAY KUMAR <akshay.2026.91@gmail.com>
2026-09-02 22:28:39 +0530

    Add Good Kid [B] from Codeforces

---
+++ 1873/B - Good Kid/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        sort(arr.begin() , arr.end());
+        arr[0] = arr[0]+1;
+        long long ans = 1;
+        for(int x:arr){
+            ans *= x;
+        }
+        cout<<ans<<endl;
+    }
+}
