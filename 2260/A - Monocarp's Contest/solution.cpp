984eb835a0e4d7240fce9b3de206efe398d3738f
AKSHAY KUMAR <akshay.2026.91@gmail.com>
2026-09-09 10:13:23 +0530

    Add Monocarp's Contest [A] from Codeforces

--- 2260/A - Monocarp's Contest/solution.cpp
+++ 2260/A - Monocarp's Contest/solution.cpp
@@ -8,29 +8,19 @@ int main(){
         int n;
         cin>>n;
         int countzero = 0;
-        int countone = 0;
         vector<int> arr(n);
         for(auto &x:arr) {
             cin>>x;
             if(x == 0){
                 countzero++;
-            }else{
-                countone++;
             }
         }
-        int count = 0;
-        bool flag = false;
-        int left = 0;
-        int right = n-1;
-        if(countzero < 2){
-                cout<<"-1"<<endl;
-            }else if(arr[left] != 0 && arr[right] != 0){
-            cout<<"2"<<endl;
-        }else if(arr[left] == 0 && arr[right] == 0){
-            cout<<"0"<<endl;
+        if(countzero<2){
+            cout<<"-1"<<endl;
         }else{
-            cout<<"1"<<endl;
+            cout<<arr[0]+arr[n-1]<<endl;
         }
+        
  
     }
 }
